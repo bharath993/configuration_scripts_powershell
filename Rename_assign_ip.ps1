ecfc2ae4e98a54b87d03fc7a69347ef2599a0a00
bharath993 <35031241+bharath993@users.noreply.github.com>
2021-10-25 18:49:37 +0530

    Add files via upload

---
+++ Rename_assign_ip.ps1
@@ -0,0 +1,69 @@
+﻿#Script to rename the chelsio adapter and assign ip to it
+function host ($name)
+{
+if($name -eq "bumblebee")
+{
+netsh interface ip add address  "port0" 102.1.7.2 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.2 255.255.255.0
+}
+elseif ($name -eq "core96cn23")
+{
+netsh interface ip add address  "port0" 102.1.7.3 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.3 255.255.255.0
+}
+elseif ($name -eq "core96cn4")
+{
+netsh interface ip add address  "port0" 102.1.5.2 255.255.255.0
+netsh interface ip add address  "port1" 102.1.6.2 255.255.255.0
+}
+elseif ($name -eq "core96cn16")
+{
+netsh interface ip add address  "port0" 102.1.5.3 255.255.255.0
+netsh interface ip add address  "port1" 102.1.6.3 255.255.255.0
+}
+elseif ($name -eq "core96cn22")
+{
+netsh interface ip add address  "port0" 102.1.7.22 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.22 255.255.255.0
+}
+elseif ($name -eq "core96cn24")
+{
+netsh interface ip add address  "port0" 102.1.7.24 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.24 255.255.255.0
+}
+elseif ($name -eq "rattletrap")
+{
+netsh interface ip add address  "port0" 102.1.7.62 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.62 255.255.255.0
+}
+elseif ($name -eq "warrior")
+{
+netsh interface ip add address  "port0" 102.1.7.56 255.255.255.0
+netsh interface ip add address  "port1" 102.1.8.56 255.255.255.0
+}
+else
+{
+Write-Host "The local host name doesn't match with any of the hostname, please provide the ip manually" -f Yellow
+$ip1 = Read-Host "Enter ip for port0" 
+netsh interface ip add address  "port0" $ip1 255.255.255.0
+$ip2 = Read-Host "Enter ip for port1" 
+netsh interface ip add address  "port1" $ip2 255.255.255.0
+}
+}
+
+$a = Get-NetAdapter -InterfaceDescription "Chel*" 
+$p = $a.macaddress -replace '[0-9,a-z]+-\w',""
+if ( $p.Get(0) -eq 0 )
+{
+Rename-NetAdapter $a.name.Get(0) -NewName "port0" -ErrorAction Ignore
+Rename-NetAdapter $a.name.Get(1) -NewName "port1" -ErrorAction Ignore
+}
+else
+{
+Rename-NetAdapter $a.name.Get(0) -NewName "port1" -ErrorAction Ignore
+Rename-NetAdapter $a.name.Get(1) -NewName "port0" -ErrorAction Ignore
+}
+$hostname = hostname
+host $hostname
+sleep 8
+ipconfig 
---
+++ creating_vm.ps1
@@ -0,0 +1,34 @@
+﻿#Script to create new vm and assign no. of processor to it 
+#By default this script assigns 1Gb of memory to each vm
+#all the vm are created with generation type 1
+param
+(
+[int]$max_no_of_vm,
+[int]$no_of_processor_count_for_each_vm,
+[string]$VHD_path
+)
+
+if (($max_no_of_vm -eq 0) -or ($no_of_processor_count_for_each_vm -eq 0) -or ($VHD_path -eq [string]$null))
+{
+write-host "Please specify all fo these  params -max_no_of_vm,-no_of_processor_count_for_each_vm,-VHD_path" -f Yellow
+pause
+exit
+}
+
+for ($i =1 ; $i -le $max_no_of_vm ; $i++)
+{
+$app = $VHD_path + "\vm$i.vhdx"
+foreach ($apps in $app){
+if((Test-VHD -Path $apps) -ne 1)
+{
+write-host "The vhd path is not correct!!!! for $apps" -f Red
+pause 
+exit
+}
+write-host "Creating vm of name vm$i and assigning $no_of_processor_count_for_each_vm processor count to it" -f Yellow
+New-VM -Name "vm$i" -MemoryStartupBytes 1GB -VHDPath $apps -Generation 1 
+sleep 2
+set-Vm -Name "vm$i" -ProcessorCount $no_of_processor_count_for_each_vm
+sleep 2
+}
+}
